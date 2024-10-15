9b0f5efb637ef2c54a9fd58f28a7ebe5b9fcbaa8
Trick3y <161294923+Trick3y@users.noreply.github.com>
2024-10-15 20:21:56 +1100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,21 @@
+# factorial.py
+
 def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-num = int(input("Enter a number: "))
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of",num,"is",recur_factorial(num))
+    """Recursively calculates the factorial of a number."""
+    if n == 0:
+        return 1
+    elif n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
+
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+        print("The factorial of 0 is 1")
+    else:
+        print("The factorial of", num, "is", recur_factorial(num))
+
